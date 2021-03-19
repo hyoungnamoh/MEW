357b3820b53188d9960e967229b5bf093dd69170
오형남 <zzzsh789@naver.com>
2021-03-19 23:23:59 +0900

    Add: react-navigation

---
+++ front/App.js
@@ -0,0 +1,165 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/page/Home';
+
+
+// In App.js in a new project
+
+function HomeScreen({ route, navigation }) {
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+    </View>
+  );
+}
+
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass params back to home screen
+          navigation.navigate('Home', { post: postText });
+        }}
+      />
+    </>
+  );
+}
+
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+
+
+  );
+}
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <NavigationContainer initialRouteName="Home">
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+          />
+          <Stack.Screen
+            name="CreatePost"
+            component={CreatePostScreen}
+          />
+          <Stack.Screen
+            name="Home2"
+            component={Home}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ front/src/page/Home.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const Home = () => {
+  const [selected, setSelcted] = useState([]);
+  useEffect(() => {
+    getButtons();
+  }, []);
+
+  const getButtons = () => {
+    const buttonArray = new Array(61).fill('').map((e, i) => {
+      return (
+        <TouchableOpacity
+          key={e + i}
+          style={{
+            backgroundColor: selected.includes(i) ? 'black' : 'white',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderColor: 'black',
+            borderRadius: 10,
+            minWidth: 100,
+            maxWidth: 100,
+            height: 40,
+            borderWidth: 1,
+            marginVertical: 10,
+            marginHorizontal: 5,
+          }}
+          onPress={() => { onPressButton(i) }}
+        >
+          <Text style={{ color: selected.includes(i) ? 'white' : 'black', }}>{`DAY ${i + 1}`}</Text>
+        </TouchableOpacity>
+      );
+    });
+    return buttonArray;
+  };
+
+  const onPressButton = (index) => {
+    const findIndex = selected.findIndex(e => e === index);
+    if (findIndex > -1) {
+      const copySelected = [...selected];
+      copySelected.splice(findIndex, 1)
+      setSelcted(copySelected);
+    } else {
+      selected.push(index);
+      setSelcted([...selected]);
+    }
+  }
+
+  const getWords = async (selected) => {
+    try {
+      const response = await axios.get('http://localhost:3603/words', {
+        params: {
+          selected: selected,
+        },
+      });
+    } catch (error) {
+      console.log('error:', error);
+    }
+  }
+
+  const onPressStart = async () => {
+    const words = await getWords(selected);
+  }
+  return (
+    <ScrollView contentContainerStyle={{ alignItems: 'center' }}>
+      <Text onPress={getButtons}>MEW</Text>
+      <TouchableOpacity onPress={onPressStart}>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </TouchableOpacity>
+      <View style={{ flexDirection: 'row', width: 400, flexWrap: 'wrap', justifyContent: 'center' }}>
+        {getButtons()}
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default Home;
